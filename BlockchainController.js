1632a9eaac26f6afc19d49be3d7baa130b09e868
Jacob <jacob.hildebrandt@gmail.com>
2021-07-13 16:09:10 -0700

    implemented validateChain

--- BlockchainController.js
+++ BlockchainController.js
@@ -17,6 +17,7 @@ class BlockchainController {
         this.submitStar();
         this.getBlockByHash();
         this.getStarsByOwner();
+        this.validateChain();
     }
 
     // Enpoint to Get a Block by Height (GET Endpoint)
@@ -131,6 +132,17 @@ class BlockchainController {
             }
             
         });
+    }
+
+    validateChain() {
+        this.app.get('/validate', async (req, res) => {
+            try {
+                const message = await this.blockchain.validateChain();
+                return res.status(200).json(message);
+            } catch (error) {
+                return res.status(500).json(error);
+            }
+        })
     }
 
 }
